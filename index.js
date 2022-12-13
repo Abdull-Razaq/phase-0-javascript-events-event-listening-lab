0c1370e90ecbdbae8e0664fca41b827f6ef69de5
Abdirizak Mohamed <abdirizakm123@gmail.com>
2022-12-13 14:48:58 +0300

    Added an event listener to my index.js

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
-document.getElementById("button").addEventListener("click", function () {
+function addingEventListener() {
+  const input = document.getElementById("button");
+
+  function clickAlert() {
     alert("I was clicked!");
-  });
+  }
+
+  input.addEventListener("click", clickAlert);
+}
+
